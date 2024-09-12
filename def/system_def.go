6527aeda0884cf776b44c40a259346f02e755756
pojol <pojol@outlook.com>
2024-09-12 22:01:55 +0800

    feat : add symbol SymbolLocalFirst

--- def/system_def.go
+++ def/system_def.go
@@ -11,6 +11,10 @@ const (
 	// 表示路由到所有 该类型的 actor
 	// - 注 这个符号只能用于 send 接口（异步调用
 	SymbolAll = "*"
+
+	// 表示随机路由到一个该类型的 actor，但优先选择本节点上的
+	// 如果本节点上没有该类型的 actor，则随机选择其他节点上的
+	SymbolLocalFirst = "~"
 )
 
 const (
